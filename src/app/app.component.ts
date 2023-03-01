de43ad6494b907b62b3f0288c2d76ba28034d090
Denis Federico Zelaya <48973732+DenisFZelaya@users.noreply.github.com>
2023-02-28 20:48:36 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Denis';
+  edad : number = 29;
+  btnDisabled = true;
+  inputName = "Denis Zelaya"
+
+   toggleButtom() {
+    this.btnDisabled  = !this.btnDisabled
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.name = element.value;
+  }
 }
